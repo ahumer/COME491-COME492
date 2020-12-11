55d08079cf7ae7c3c3dbe9d6e7e86d779864a37b
ahumer <ahumer@gmail.com>
2020-12-11 20:34:22 +0300

    Basic BlueTooth usage by mobile app.
    Controlling led via BlueTooth module on Arduino board by using a mobile application that coded with MIT App Inventor.

---
+++ BluetoothBasic/BluetoothBasic.ino
@@ -0,0 +1,24 @@
+/*
+ * Controlling led via BlueTooth by using a mobile application coded with MIT App Inventor.
+ */
+#define LED 13
+char sendedData = 0;
+ 
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  while(Serial.available()==0)
+  {
+  }
+  sendedData = Serial.read();
+  Serial.println(sendedData);
+
+  if(sendedData=='1')
+    digitalWrite(LED,HIGH);
+  else if(sendedData=='0')
+    digitalWrite(LED,LOW);
+ 
+}
