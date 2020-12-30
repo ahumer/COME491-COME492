10aaa4d46b040946179b62266052299c1b72fa3f
ahumer <ahumer@gmail.com>
2020-12-30 18:18:07 +0300

    Redesigning of Main Window
    It is adapted to the interface design in the presentation and some additions were made that were not present in the presentation but were considered necessary.

--- Central Management/Form1.Designer.cs
+++ Central Management/Form1.Designer.cs
@@ -34,21 +34,29 @@
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.OPENbtn = new System.Windows.Forms.Button();
             this.CLOSEbtn = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.sysGrBx = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.StateLBL = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.PortTB = new System.Windows.Forms.TextBox();
-            this.groupBox1.SuspendLayout();
+            this.sysInf = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblSysHeader = new System.Windows.Forms.Label();
+            this.lblSys = new System.Windows.Forms.Label();
+            this.lblRbt = new System.Windows.Forms.Label();
+            this.lblSensor = new System.Windows.Forms.Label();
+            this.lblCntHeader = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sysGrBx.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // ONbtn
             // 
             this.ONbtn.Enabled = false;
-            this.ONbtn.Location = new System.Drawing.Point(10, 33);
+            this.ONbtn.Location = new System.Drawing.Point(10, 28);
             this.ONbtn.Name = "ONbtn";
-            this.ONbtn.Size = new System.Drawing.Size(75, 23);
+            this.ONbtn.Size = new System.Drawing.Size(90, 23);
             this.ONbtn.TabIndex = 0;
             this.ONbtn.Text = "ON";
             this.ONbtn.UseVisualStyleBackColor = true;
@@ -57,9 +65,9 @@
             // OFFbtn
             // 
             this.OFFbtn.Enabled = false;
-            this.OFFbtn.Location = new System.Drawing.Point(25, 33);
+            this.OFFbtn.Location = new System.Drawing.Point(10, 12);
             this.OFFbtn.Name = "OFFbtn";
-            this.OFFbtn.Size = new System.Drawing.Size(75, 23);
+            this.OFFbtn.Size = new System.Drawing.Size(90, 23);
             this.OFFbtn.TabIndex = 1;
             this.OFFbtn.Text = "OFF";
             this.OFFbtn.UseVisualStyleBackColor = true;
@@ -67,7 +75,7 @@
             // 
             // OPENbtn
             // 
-            this.OPENbtn.Location = new System.Drawing.Point(10, 122);
+            this.OPENbtn.Location = new System.Drawing.Point(10, 108);
             this.OPENbtn.Name = "OPENbtn";
             this.OPENbtn.Size = new System.Drawing.Size(75, 23);
             this.OPENbtn.TabIndex = 2;
@@ -78,7 +86,7 @@
             // CLOSEbtn
             // 
             this.CLOSEbtn.Enabled = false;
-            this.CLOSEbtn.Location = new System.Drawing.Point(25, 122);
+            this.CLOSEbtn.Location = new System.Drawing.Point(10, 126);
             this.CLOSEbtn.Name = "CLOSEbtn";
             this.CLOSEbtn.Size = new System.Drawing.Size(75, 23);
             this.CLOSEbtn.TabIndex = 3;
@@ -86,45 +94,53 @@
             this.CLOSEbtn.UseVisualStyleBackColor = true;
             this.CLOSEbtn.Click += new System.EventHandler(this.CLOSEbtn_Click);
             // 
-            // groupBox1
+            // sysGrBx
             // 
-            this.groupBox1.Controls.Add(this.ONbtn);
-            this.groupBox1.Controls.Add(this.OFFbtn);
-            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.groupBox1.Location = new System.Drawing.Point(0, 0);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox1.Size = new System.Drawing.Size(262, 81);
-            this.groupBox1.TabIndex = 8;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "LED CONTROL";
+            this.sysGrBx.Controls.Add(this.label2);
+            this.sysGrBx.Controls.Add(this.lblSensor);
+            this.sysGrBx.Controls.Add(this.lblRbt);
+            this.sysGrBx.Controls.Add(this.lblSys);
+            this.sysGrBx.Controls.Add(this.lblSysHeader);
+            this.sysGrBx.Controls.Add(this.button1);
+            this.sysGrBx.Controls.Add(this.sysInf);
+            this.sysGrBx.Controls.Add(this.ONbtn);
+            this.sysGrBx.Controls.Add(this.OFFbtn);
+            this.sysGrBx.Dock = System.Windows.Forms.DockStyle.Top;
+            this.sysGrBx.Location = new System.Drawing.Point(0, 0);
+            this.sysGrBx.Margin = new System.Windows.Forms.Padding(2);
+            this.sysGrBx.Name = "sysGrBx";
+            this.sysGrBx.Padding = new System.Windows.Forms.Padding(2);
+            this.sysGrBx.Size = new System.Drawing.Size(403, 150);
+            this.sysGrBx.TabIndex = 8;
+            this.sysGrBx.TabStop = false;
+            this.sysGrBx.Text = "SYSTEM CONTROL";
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.StateLBL);
+            this.groupBox2.Controls.Add(this.lblCntHeader);
+            this.groupBox2.Controls.Add(this.lblState);
             this.groupBox2.Controls.Add(this.label1);
             this.groupBox2.Controls.Add(this.PortTB);
             this.groupBox2.Controls.Add(this.OPENbtn);
             this.groupBox2.Controls.Add(this.CLOSEbtn);
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.groupBox2.Location = new System.Drawing.Point(0, 86);
+            this.groupBox2.Location = new System.Drawing.Point(0, 154);
             this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
-            this.groupBox2.Size = new System.Drawing.Size(262, 172);
+            this.groupBox2.Size = new System.Drawing.Size(403, 172);
             this.groupBox2.TabIndex = 9;
             this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "SERIAL CONTROL";
+            this.groupBox2.Text = "CONNECTION CONTROL";
             // 
-            // StateLBL
+            // lblState
             // 
-            this.StateLBL.AutoSize = true;
-            this.StateLBL.Location = new System.Drawing.Point(161, 37);
-            this.StateLBL.Name = "StateLBL";
-            this.StateLBL.Size = new System.Drawing.Size(78, 13);
-            this.StateLBL.TabIndex = 6;
-            this.StateLBL.Text = "Not connected";
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(253, 70);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(78, 13);
+            this.lblState.TabIndex = 6;
+            this.lblState.Text = "Not connected";
             // 
             // label1
             // 
@@ -144,16 +160,90 @@
             this.PortTB.Size = new System.Drawing.Size(76, 20);
             this.PortTB.TabIndex = 4;
             // 
+            // sysInf
+            // 
+            this.sysInf.Location = new System.Drawing.Point(10, 57);
+            this.sysInf.Name = "sysInf";
+            this.sysInf.Size = new System.Drawing.Size(90, 23);
+            this.sysInf.TabIndex = 7;
+            this.sysInf.Text = "System Info";
+            this.sysInf.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 35);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Location Configuration";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lblSysHeader
+            // 
+            this.lblSysHeader.AutoSize = true;
+            this.lblSysHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSysHeader.Location = new System.Drawing.Point(224, 15);
+            this.lblSysHeader.Name = "lblSysHeader";
+            this.lblSysHeader.Size = new System.Drawing.Size(113, 20);
+            this.lblSysHeader.TabIndex = 9;
+            this.lblSysHeader.Text = "System Status";
+            // 
+            // lblSys
+            // 
+            this.lblSys.AutoSize = true;
+            this.lblSys.Location = new System.Drawing.Point(232, 57);
+            this.lblSys.Name = "lblSys";
+            this.lblSys.Size = new System.Drawing.Size(27, 13);
+            this.lblSys.TabIndex = 10;
+            this.lblSys.Text = "OFF";
+            // 
+            // lblRbt
+            // 
+            this.lblRbt.AutoSize = true;
+            this.lblRbt.Location = new System.Drawing.Point(232, 87);
+            this.lblRbt.Name = "lblRbt";
+            this.lblRbt.Size = new System.Drawing.Size(131, 13);
+            this.lblRbt.TabIndex = 11;
+            this.lblRbt.Text = "Robot arm: not connected";
+            // 
+            // lblSensor
+            // 
+            this.lblSensor.AutoSize = true;
+            this.lblSensor.Location = new System.Drawing.Point(0, 0);
+            this.lblSensor.Name = "lblSensor";
+            this.lblSensor.Size = new System.Drawing.Size(0, 13);
+            this.lblSensor.TabIndex = 12;
+            // 
+            // lblCntHeader
+            // 
+            this.lblCntHeader.AutoSize = true;
+            this.lblCntHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCntHeader.Location = new System.Drawing.Point(224, 15);
+            this.lblCntHeader.Name = "lblCntHeader";
+            this.lblCntHeader.Size = new System.Drawing.Size(141, 20);
+            this.lblCntHeader.TabIndex = 7;
+            this.lblCntHeader.Text = "Connection Status";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "label2";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(262, 258);
+            this.ClientSize = new System.Drawing.Size(403, 326);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.sysGrBx);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.groupBox1.ResumeLayout(false);
+            this.sysGrBx.ResumeLayout(false);
+            this.sysGrBx.PerformLayout();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
@@ -167,11 +257,19 @@
         private System.IO.Ports.SerialPort serialPort1;
         private System.Windows.Forms.Button OPENbtn;
         private System.Windows.Forms.Button CLOSEbtn;
-        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox sysGrBx;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.TextBox PortTB;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label StateLBL;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label lblSysHeader;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button sysInf;
+        private System.Windows.Forms.Label lblSys;
+        private System.Windows.Forms.Label lblSensor;
+        private System.Windows.Forms.Label lblRbt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCntHeader;
     }
 }
 
