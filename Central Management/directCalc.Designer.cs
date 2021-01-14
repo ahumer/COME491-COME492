b34f803aca034862d8b18cb192ddc21d11d02968
ahumer <ahumer@gmail.com>
2021-01-14 23:48:16 +0300

    Designing "Direction Settings" Window
    There is a group box for radio buttons of locations. There is another group box for functions. In this group box, there are textboxes, a label to show the vehicle location, four buttons, respectively for calculation of the path, showing IDs of the locations, with labels under radio buttons, and assigning new card IDs to locations.
    The user selects a location, and select the radio button of the vehicle position, then the "calculation" button turns to "enabled", and then click on the button, it calculates the path, and writes the direction to the text box.
    The user selects a location, and select the radio button for the new card ID. Then "Assign ID" button turns to enabled. Then enter the card ID into four textboxes, each of them for one number group of the ID. And click the button.
    Show ID button shows the ID of each location, on labels that under radiobuttons.

---
+++ Central Management/directCalc.Designer.cs
@@ -0,0 +1,454 @@
+﻿namespace Central_Management
+{
+    partial class directCal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbLocation = new System.Windows.Forms.GroupBox();
+            this.lblJ = new System.Windows.Forms.Label();
+            this.lblF = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.lblH = new System.Windows.Forms.Label();
+            this.lblE = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblG = new System.Windows.Forms.Label();
+            this.lblD = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.rbJ = new System.Windows.Forms.RadioButton();
+            this.rbH = new System.Windows.Forms.RadioButton();
+            this.rbG = new System.Windows.Forms.RadioButton();
+            this.rbF = new System.Windows.Forms.RadioButton();
+            this.rbE = new System.Windows.Forms.RadioButton();
+            this.rbD = new System.Windows.Forms.RadioButton();
+            this.rbC = new System.Windows.Forms.RadioButton();
+            this.rbB = new System.Windows.Forms.RadioButton();
+            this.rbA = new System.Windows.Forms.RadioButton();
+            this.gbFunctions = new System.Windows.Forms.GroupBox();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnAssign = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.tbID1 = new System.Windows.Forms.TextBox();
+            this.tbID2 = new System.Windows.Forms.TextBox();
+            this.tbID3 = new System.Windows.Forms.TextBox();
+            this.tbID4 = new System.Windows.Forms.TextBox();
+            this.rbVehicle = new System.Windows.Forms.RadioButton();
+            this.rbCardID = new System.Windows.Forms.RadioButton();
+            this.gbLocation.SuspendLayout();
+            this.gbFunctions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbLocation
+            // 
+            this.gbLocation.Controls.Add(this.lblJ);
+            this.gbLocation.Controls.Add(this.lblF);
+            this.gbLocation.Controls.Add(this.lblC);
+            this.gbLocation.Controls.Add(this.lblH);
+            this.gbLocation.Controls.Add(this.lblE);
+            this.gbLocation.Controls.Add(this.lblB);
+            this.gbLocation.Controls.Add(this.lblG);
+            this.gbLocation.Controls.Add(this.lblD);
+            this.gbLocation.Controls.Add(this.lblA);
+            this.gbLocation.Controls.Add(this.rbJ);
+            this.gbLocation.Controls.Add(this.rbH);
+            this.gbLocation.Controls.Add(this.rbG);
+            this.gbLocation.Controls.Add(this.rbF);
+            this.gbLocation.Controls.Add(this.rbE);
+            this.gbLocation.Controls.Add(this.rbD);
+            this.gbLocation.Controls.Add(this.rbC);
+            this.gbLocation.Controls.Add(this.rbB);
+            this.gbLocation.Controls.Add(this.rbA);
+            this.gbLocation.Location = new System.Drawing.Point(12, 12);
+            this.gbLocation.Name = "gbLocation";
+            this.gbLocation.Size = new System.Drawing.Size(442, 245);
+            this.gbLocation.TabIndex = 0;
+            this.gbLocation.TabStop = false;
+            this.gbLocation.Text = "Select Locaiton";
+            // 
+            // lblJ
+            // 
+            this.lblJ.AutoSize = true;
+            this.lblJ.Location = new System.Drawing.Point(344, 209);
+            this.lblJ.Name = "lblJ";
+            this.lblJ.Size = new System.Drawing.Size(35, 13);
+            this.lblJ.TabIndex = 17;
+            this.lblJ.Text = "label1";
+            // 
+            // lblF
+            // 
+            this.lblF.AutoSize = true;
+            this.lblF.Location = new System.Drawing.Point(175, 209);
+            this.lblF.Name = "lblF";
+            this.lblF.Size = new System.Drawing.Size(35, 13);
+            this.lblF.TabIndex = 16;
+            this.lblF.Text = "label1";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Location = new System.Drawing.Point(6, 209);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(35, 13);
+            this.lblC.TabIndex = 15;
+            this.lblC.Text = "label1";
+            // 
+            // lblH
+            // 
+            this.lblH.AutoSize = true;
+            this.lblH.Location = new System.Drawing.Point(344, 127);
+            this.lblH.Name = "lblH";
+            this.lblH.Size = new System.Drawing.Size(35, 13);
+            this.lblH.TabIndex = 14;
+            this.lblH.Text = "label1";
+            // 
+            // lblE
+            // 
+            this.lblE.AutoSize = true;
+            this.lblE.Location = new System.Drawing.Point(175, 127);
+            this.lblE.Name = "lblE";
+            this.lblE.Size = new System.Drawing.Size(35, 13);
+            this.lblE.TabIndex = 13;
+            this.lblE.Text = "label1";
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(6, 127);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(35, 13);
+            this.lblB.TabIndex = 12;
+            this.lblB.Text = "label1";
+            // 
+            // lblG
+            // 
+            this.lblG.AutoSize = true;
+            this.lblG.Location = new System.Drawing.Point(344, 57);
+            this.lblG.Name = "lblG";
+            this.lblG.Size = new System.Drawing.Size(35, 13);
+            this.lblG.TabIndex = 11;
+            this.lblG.Text = "label1";
+            // 
+            // lblD
+            // 
+            this.lblD.AutoSize = true;
+            this.lblD.Location = new System.Drawing.Point(175, 57);
+            this.lblD.Name = "lblD";
+            this.lblD.Size = new System.Drawing.Size(35, 13);
+            this.lblD.TabIndex = 10;
+            this.lblD.Text = "label1";
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(6, 57);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(35, 13);
+            this.lblA.TabIndex = 9;
+            this.lblA.Text = "label1";
+            // 
+            // rbJ
+            // 
+            this.rbJ.AutoSize = true;
+            this.rbJ.Location = new System.Drawing.Point(347, 169);
+            this.rbJ.Name = "rbJ";
+            this.rbJ.Size = new System.Drawing.Size(74, 17);
+            this.rbJ.TabIndex = 8;
+            this.rbJ.TabStop = true;
+            this.rbJ.Text = "Location J";
+            this.rbJ.UseVisualStyleBackColor = true;
+            // 
+            // rbH
+            // 
+            this.rbH.AutoSize = true;
+            this.rbH.Location = new System.Drawing.Point(347, 93);
+            this.rbH.Name = "rbH";
+            this.rbH.Size = new System.Drawing.Size(80, 17);
+            this.rbH.TabIndex = 7;
+            this.rbH.TabStop = true;
+            this.rbH.Text = " Location H";
+            this.rbH.UseVisualStyleBackColor = true;
+            // 
+            // rbG
+            // 
+            this.rbG.AutoSize = true;
+            this.rbG.Location = new System.Drawing.Point(347, 28);
+            this.rbG.Name = "rbG";
+            this.rbG.Size = new System.Drawing.Size(77, 17);
+            this.rbG.TabIndex = 6;
+            this.rbG.TabStop = true;
+            this.rbG.Text = "Location G";
+            this.rbG.UseVisualStyleBackColor = true;
+            // 
+            // rbF
+            // 
+            this.rbF.AutoSize = true;
+            this.rbF.Location = new System.Drawing.Point(177, 169);
+            this.rbF.Name = "rbF";
+            this.rbF.Size = new System.Drawing.Size(75, 17);
+            this.rbF.TabIndex = 5;
+            this.rbF.TabStop = true;
+            this.rbF.Text = "Location F";
+            this.rbF.UseVisualStyleBackColor = true;
+            // 
+            // rbE
+            // 
+            this.rbE.AutoSize = true;
+            this.rbE.Location = new System.Drawing.Point(177, 93);
+            this.rbE.Name = "rbE";
+            this.rbE.Size = new System.Drawing.Size(76, 17);
+            this.rbE.TabIndex = 4;
+            this.rbE.TabStop = true;
+            this.rbE.Text = "Location E";
+            this.rbE.UseVisualStyleBackColor = true;
+            // 
+            // rbD
+            // 
+            this.rbD.AutoSize = true;
+            this.rbD.Location = new System.Drawing.Point(178, 28);
+            this.rbD.Name = "rbD";
+            this.rbD.Size = new System.Drawing.Size(77, 17);
+            this.rbD.TabIndex = 3;
+            this.rbD.TabStop = true;
+            this.rbD.Text = "Location D";
+            this.rbD.UseVisualStyleBackColor = true;
+            // 
+            // rbC
+            // 
+            this.rbC.AutoSize = true;
+            this.rbC.Location = new System.Drawing.Point(9, 169);
+            this.rbC.Name = "rbC";
+            this.rbC.Size = new System.Drawing.Size(76, 17);
+            this.rbC.TabIndex = 2;
+            this.rbC.TabStop = true;
+            this.rbC.Text = "Location C";
+            this.rbC.UseVisualStyleBackColor = true;
+            // 
+            // rbB
+            // 
+            this.rbB.AutoSize = true;
+            this.rbB.Location = new System.Drawing.Point(9, 93);
+            this.rbB.Name = "rbB";
+            this.rbB.Size = new System.Drawing.Size(76, 17);
+            this.rbB.TabIndex = 1;
+            this.rbB.TabStop = true;
+            this.rbB.Text = "Location B";
+            this.rbB.UseVisualStyleBackColor = true;
+            // 
+            // rbA
+            // 
+            this.rbA.AutoSize = true;
+            this.rbA.Location = new System.Drawing.Point(9, 28);
+            this.rbA.Name = "rbA";
+            this.rbA.Size = new System.Drawing.Size(76, 17);
+            this.rbA.TabIndex = 0;
+            this.rbA.TabStop = true;
+            this.rbA.Text = "Location A";
+            this.rbA.UseVisualStyleBackColor = true;
+            // 
+            // gbFunctions
+            // 
+            this.gbFunctions.Controls.Add(this.rbCardID);
+            this.gbFunctions.Controls.Add(this.rbVehicle);
+            this.gbFunctions.Controls.Add(this.tbID4);
+            this.gbFunctions.Controls.Add(this.tbID3);
+            this.gbFunctions.Controls.Add(this.tbID2);
+            this.gbFunctions.Controls.Add(this.tbID1);
+            this.gbFunctions.Controls.Add(this.btnShow);
+            this.gbFunctions.Controls.Add(this.btnAssign);
+            this.gbFunctions.Controls.Add(this.btnClose);
+            this.gbFunctions.Controls.Add(this.richTextBox1);
+            this.gbFunctions.Controls.Add(this.btnCalc);
+            this.gbFunctions.Location = new System.Drawing.Point(12, 263);
+            this.gbFunctions.Name = "gbFunctions";
+            this.gbFunctions.Size = new System.Drawing.Size(442, 288);
+            this.gbFunctions.TabIndex = 1;
+            this.gbFunctions.TabStop = false;
+            this.gbFunctions.Text = "Functions";
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(206, 241);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(63, 23);
+            this.btnShow.TabIndex = 6;
+            this.btnShow.Text = "Show IDs";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnAssign
+            // 
+            this.btnAssign.Location = new System.Drawing.Point(90, 241);
+            this.btnAssign.Name = "btnAssign";
+            this.btnAssign.Size = new System.Drawing.Size(96, 23);
+            this.btnAssign.TabIndex = 5;
+            this.btnAssign.Text = "Assign Card ID";
+            this.btnAssign.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(382, 241);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(54, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(7, 76);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(429, 140);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Enabled = false;
+            this.btnCalc.Location = new System.Drawing.Point(7, 241);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(62, 23);
+            this.btnCalc.TabIndex = 0;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            // 
+            // tbID1
+            // 
+            this.tbID1.Enabled = false;
+            this.tbID1.Location = new System.Drawing.Point(294, 26);
+            this.tbID1.MaxLength = 3;
+            this.tbID1.Name = "tbID1";
+            this.tbID1.Size = new System.Drawing.Size(31, 20);
+            this.tbID1.TabIndex = 7;
+            this.tbID1.TextChanged += new System.EventHandler(this.tbID1_TextChanged);
+            // 
+            // tbID2
+            // 
+            this.tbID2.Enabled = false;
+            this.tbID2.Location = new System.Drawing.Point(331, 26);
+            this.tbID2.MaxLength = 3;
+            this.tbID2.Name = "tbID2";
+            this.tbID2.Size = new System.Drawing.Size(31, 20);
+            this.tbID2.TabIndex = 8;
+            // 
+            // tbID3
+            // 
+            this.tbID3.Enabled = false;
+            this.tbID3.Location = new System.Drawing.Point(368, 26);
+            this.tbID3.MaxLength = 3;
+            this.tbID3.Name = "tbID3";
+            this.tbID3.Size = new System.Drawing.Size(31, 20);
+            this.tbID3.TabIndex = 9;
+            // 
+            // tbID4
+            // 
+            this.tbID4.Enabled = false;
+            this.tbID4.Location = new System.Drawing.Point(405, 26);
+            this.tbID4.MaxLength = 3;
+            this.tbID4.Name = "tbID4";
+            this.tbID4.Size = new System.Drawing.Size(31, 20);
+            this.tbID4.TabIndex = 10;
+            // 
+            // rbVehicle
+            // 
+            this.rbVehicle.AutoSize = true;
+            this.rbVehicle.Location = new System.Drawing.Point(9, 27);
+            this.rbVehicle.Name = "rbVehicle";
+            this.rbVehicle.Size = new System.Drawing.Size(159, 17);
+            this.rbVehicle.TabIndex = 12;
+            this.rbVehicle.TabStop = true;
+            this.rbVehicle.Text = "Vehicle position : Location #";
+            this.rbVehicle.UseVisualStyleBackColor = true;
+            this.rbVehicle.Click += new System.EventHandler(this.rbVehicle_Click);
+            // 
+            // rbCardID
+            // 
+            this.rbCardID.AutoSize = true;
+            this.rbCardID.Location = new System.Drawing.Point(177, 26);
+            this.rbCardID.Name = "rbCardID";
+            this.rbCardID.Size = new System.Drawing.Size(114, 17);
+            this.rbCardID.TabIndex = 13;
+            this.rbCardID.TabStop = true;
+            this.rbCardID.Text = "Enter New Card ID";
+            this.rbCardID.UseVisualStyleBackColor = true;
+            this.rbCardID.Click += new System.EventHandler(this.rbCardID_Click);
+            // 
+            // directCal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 561);
+            this.Controls.Add(this.gbFunctions);
+            this.Controls.Add(this.gbLocation);
+            this.Name = "directCal";
+            this.Text = "Direction Settings";
+            this.Load += new System.EventHandler(this.directCalc_Load);
+            this.gbLocation.ResumeLayout(false);
+            this.gbLocation.PerformLayout();
+            this.gbFunctions.ResumeLayout(false);
+            this.gbFunctions.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbLocation;
+        private System.Windows.Forms.RadioButton rbJ;
+        private System.Windows.Forms.RadioButton rbH;
+        private System.Windows.Forms.RadioButton rbG;
+        private System.Windows.Forms.RadioButton rbF;
+        private System.Windows.Forms.RadioButton rbE;
+        private System.Windows.Forms.RadioButton rbD;
+        private System.Windows.Forms.RadioButton rbC;
+        private System.Windows.Forms.RadioButton rbB;
+        private System.Windows.Forms.RadioButton rbA;
+        private System.Windows.Forms.GroupBox gbFunctions;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label lblJ;
+        private System.Windows.Forms.Label lblF;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.Label lblH;
+        private System.Windows.Forms.Label lblE;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblG;
+        private System.Windows.Forms.Label lblD;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnAssign;
+        private System.Windows.Forms.TextBox tbID1;
+        private System.Windows.Forms.TextBox tbID4;
+        private System.Windows.Forms.TextBox tbID3;
+        private System.Windows.Forms.TextBox tbID2;
+        private System.Windows.Forms.RadioButton rbCardID;
+        private System.Windows.Forms.RadioButton rbVehicle;
+    }
+}
---
+++ Central Management/directCalc.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Central_Management
+{
+    public partial class directCal : Form
+    {
+        bool controlShowBtn = false;
+        public directCal()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Form frm = this.FindForm();
+            frm.Close();
+        }
+
+        private void directCalc_Load(object sender, EventArgs e)
+        {
+            lblVposition.Text = "";
+            lblA.Visible = false;
+            lblB.Visible = false;
+            lblC.Visible = false;
+            lblD.Visible = false;
+            lblE.Visible = false;
+            lblF.Visible = false;
+            lblG.Visible = false;
+            lblH.Visible = false;
+            lblJ.Visible = false;
+        }
+
+       
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            if (controlShowBtn == false)
+            {
+                this.Text = "Hide IDs";
+                controlShowBtn = true;
+                lblA.Visible = true;
+                lblB.Visible = true;
+                lblC.Visible = true;
+                lblD.Visible = true;
+                lblE.Visible = true;
+                lblF.Visible = true;
+                lblG.Visible = true;
+                lblH.Visible = true;
+                lblJ.Visible = true;
+            }
+            else
+            {
+                this.Text = "Show IDs";
+                controlShowBtn = false;
+                lblA.Visible = false;
+                lblB.Visible = false;
+                lblC.Visible = false;
+                lblD.Visible = false;
+                lblE.Visible = false;
+                lblF.Visible = false;
+                lblG.Visible = false;
+                lblH.Visible = false;
+                lblJ.Visible = false;
+            }
+            
+        }
+
+        private void tbID1_TextChanged(object sender, EventArgs e)
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, "[^0-9]"))
+            {
+                MessageBox.Show("Please enter only numbers.");
+                //textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+            }
+        }
+
+        private void rbVehicle_Click(object sender, EventArgs e)
+        {
+            btnCalc.Enabled = true;
+        }
+
+        private void rbCardID_Click(object sender, EventArgs e)
+        {
+            tbID1.Enabled = true;
+            tbID2.Enabled = true;
+            tbID3.Enabled = true;
+            tbID4.Enabled = true;
+        }
+    }
+}
