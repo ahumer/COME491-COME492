a84c1d989d381ca1ae7d04150d493d50a987b41a
ahumer <ahumer@gmail.com>
2021-01-16 03:20:30 +0300

    For Card ID Idata used in more than one  "form" class
    These data used in both main form and  direction calculation form.

---
+++ Central Management/cardIDreference.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Central_Management
+{
+    public static class cardIDreference
+    {
+        
+        public static string cardID = "";
+        public static string preCardID = "";
+        public static string[] CardIDArray = { "","","","","","","","",""};
+    }
+}
