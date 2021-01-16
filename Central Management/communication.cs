2912b3fe92dcfa980d1e667c6228e953c1506d9b
ahumer <ahumer@gmail.com>
2021-01-16 19:15:33 +0300

    Adding "Communication" Class
    "SerialConnection" function and "systemCommunication" function are transfered to this class from Form1(main form).

---
+++ Central Management/communication.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Central_Management
+{
+    public static class communication
+    {
+        public static bool SerialConnection (SerialPort port, string portName)
+        {
+            bool state = false;
+            try
+            {
+                port.PortName = portName;
+                port.BaudRate = 9600;
+                port.Open();
+                port.ReadTimeout = 5000;
+                port.WriteTimeout = 5000;
+
+                state = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            return state;
+        }
+
+        public static string systemCommunication(SerialPort port, string message = "null", bool onlyWrite = true)
+        {
+            string buffer = "";
+            short counter = 0;
+            if (message != "null")
+            {
+                try
+                {
+                    port.Write(message);
+                }
+                catch (TimeoutException)
+                {
+                    MessageBox.Show("Serial port writing time out");
+                }
+
+
+            }
+            if (onlyWrite == false)
+            {
+                while (counter < 10)
+                {
+                    if (port.BytesToRead == 0)
+                    {
+                        Thread.Sleep(1000);
+                        counter++;
+
+                    }
+
+                    if (port.BytesToRead > 2)
+                    {
+                        try
+                        {
+                            buffer = port.ReadLine();
+                            //tbSerial.Text += "sysCom"+ buffer;
+                            counter = 11;
+                        }
+                        catch (TimeoutException)
+                        {
+                            MessageBox.Show("Serial port reading time out");
+                        }
+
+                    }
+                    else
+                    {
+                        Thread.Sleep(1000);
+                        counter++;
+                    }
+                }
+
+                if (buffer == "OK")
+                {
+                    try
+                    {
+                        port.Write("002");
+                    }
+                    catch (TimeoutException)
+                    {
+                        MessageBox.Show("Serial port writing time out (start)");
+                    }
+                }
+            }
+
+
+
+            return buffer;
+
+        }
+    }
+}
